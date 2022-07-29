a2c7acdf3f60545333e5311f2c24f46777a59481
Lakshya Saxena <saxenalakshya125@gmail.com>
2022-07-29 18:50:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to PHOTO-INSTA.');
+});
 
 const CONNECTION_URL = 'mongodb+srv://admin-lakshya:Admin123@cluster0.ig36q.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
